337047d03fa2c3b0d15c6c6cef8930358a10ce0e
lipengfi <1041349167@qq.com>
2017-03-22 11:16:09 +0800

    Delete delete.handle.php

--- delete.handle.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once '../connect.php';//连接数据库
-    $name1="zhangsan";    //此处的$name为想要删除的名字
-    $sql="DELETE FROM library WHERE name='$name1'";//调用数据库 删除这个名字
-    if(mysqli_query($con, $sql))
-    {
-        echo "delete ".$name1." success!";  //删除成功
-    }
-    
