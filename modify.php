f99428c041c2d4e58ed3e93e20feb75098e45273
lipengfi <1041349167@qq.com>
2017-03-22 11:16:24 +0800

    Delete modify.php

--- modify.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once '../connect.php';//连接数据库
-    $name="lisi";//将lisi这个名字赋值给 $name
-    $sql="UPDATE library SET content='content is modified' WHERE name='$name'";
-    if(mysqli_query($con, $sql))
-    {
-        echo "modify success!";//成功
-    }
